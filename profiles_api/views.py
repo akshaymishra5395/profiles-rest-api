da5c8ba7b10bfeb5cb45a48ca6f764fdb4fa08ab
akshay <akshaymishra5395@gmail.com>
2019-12-23 08:06:23 +0530

    added the Serilizer to hello api and post method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test Api view"""
+    serializers_class = serializers.HelloSerializer
 
     def get(self,request,format=None):
         """Return a list of APIview features"""
@@ -14,4 +17,17 @@ class HelloApiView(APIView):
             'Is mapped ,anually t urls',
         ]
         return Response({'message':'Hello','an_apiview':an_apiview})
+    
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        return Response(
+            serializer.errors,status,
+            status=status.HTTP_400_BAD_REQUEST
+            )
 
