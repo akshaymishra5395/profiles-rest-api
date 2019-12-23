ff34c60e21a136f437cf4e7444bc4f7ea15aad91
akshay <akshaymishra5395@gmail.com>
2019-12-23 08:16:16 +0530

    added the Serilizer to hello api and put ,patch and delete  method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -30,4 +30,18 @@ class HelloApiView(APIView):
             serializer.errors,status,
             status=status.HTTP_400_BAD_REQUEST
             )
+    
+    def put(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'method':'PUT'})
+    
+    def patch(self,request,pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        """Handle a delete an object"""
+        return Response({'method':'DELETE'})
+
+
 
