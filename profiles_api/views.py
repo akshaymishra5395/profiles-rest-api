b16618c4149d961db76986b25af7b315971f9ad7
akshay <akshaymishra5395@gmail.com>
2019-12-23 09:28:26 +0530

    added the profile viewset,serializer,auth,filter

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test Api view"""
@@ -85,3 +91,11 @@ class HelloViewSet(viewsets.ViewSet):
             {"http_method":'Delete'}
         )
     
+class UserProfileViewset(viewsets.ModelViewSet):
+    """Handle cretaing and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,) 
+    search_fields = ('name','email',)
